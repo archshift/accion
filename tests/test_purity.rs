bd9cd2a1aa23f94618a9d4ae7e4188915430b45a
Gui Andrade <gh@archshift.com>
2020-11-29 21:42:50 -0800

    Add some proper purity analysis tests

---
+++ tests/test_purity.rs
@@ -0,0 +1,108 @@
+use accion::parser;
+use accion::ast::{self, AstNodeWrap};
+use accion::analysis::{scoping, purity};
+use accion::builtins;
+use accion::types;
+use types::Purity;
+
+struct TestPurityCtx {
+    ast: &'static ast::Ast,
+    purity: purity::Purities,
+}
+
+impl TestPurityCtx {
+    fn analyze(program: &str) -> Self {
+        let ast = parser::parse(parser::ParseInput::StrBuf(program));
+        let ast = ast.unwrap();
+    
+        let mut types = types::TypeStore::new();
+        let builtins = builtins::make_builtins(&mut types);
+        let scopes = scoping::analyze(ast, builtins);
+        let purity = purity::analyze(ast, &scopes);
+        Self {
+            ast,
+            purity,
+        }
+    }
+
+    fn assert_impure<T: AstNodeWrap>(&self, node: T) {
+        assert_eq!(self.purity.node_purity(node.as_any()), Some(Purity::Impure));
+    }
+
+    fn assert_pure<T: AstNodeWrap>(&self, node: T) {
+        assert_ne!(self.purity.node_purity(node.as_any()), Some(Purity::Impure));
+    }
+}
+
+macro_rules! unwrap_node {
+    ($node:expr, $ty:ident) => {{
+        if let ast::AstNode::$ty(inner) = $node.as_any() { inner }
+        else { panic!("Expected AST node {} but got val {:?}", stringify!($ty), $node) }
+    }};
+}
+
+macro_rules! fndecl_check_name {
+    ($node:expr, $name:expr) => {
+        assert_eq!($node.name().unwrap().name(), $name);
+    };
+}
+
+#[test]
+fn pure_valued_impure_fn() {
+    let program = r"
+        print!(x) := debug(x)
+    ";
+    let ctx = TestPurityCtx::analyze(program);
+
+    let decls: Vec<_> = ctx.ast.decls().collect();
+    let print = unwrap_node!(decls[0], ExprFnDecl);
+    ctx.assert_pure(print);
+    ctx.assert_pure(*print.val());
+}
+
+#[test]
+fn impure_valued_impure_fn() {
+    let program = r"
+        print!(x) := debug(x)
+        print2!(x) := print!(x)
+    ";
+    let ctx = TestPurityCtx::analyze(program);
+
+    let decls: Vec<_> = ctx.ast.decls().collect();
+    let print2 = unwrap_node!(decls[1], ExprFnDecl);
+    fndecl_check_name!(print2, "print2");
+    ctx.assert_pure(print2);
+    ctx.assert_impure(*print2.val());
+}
+
+#[test]
+#[should_panic]
+fn impure_valued_pure_fn() {
+    let program = r"
+        print!(x) := debug(x)
+        print2(x) := print!(x)
+    ";
+    TestPurityCtx::analyze(program);
+}
+
+#[test]
+fn pure_global() {
+    let program = r#"
+        printed := debug("hello")
+    "#;
+    let ctx = TestPurityCtx::analyze(program);
+
+    let decls: Vec<_> = ctx.ast.decls().collect();
+    let printed = unwrap_node!(decls[0], ExprVarDecl);
+    ctx.assert_pure(printed);
+}
+
+#[test]
+#[should_panic]
+fn impure_global() {
+    let program = r#"
+        print!(x) := debug(x)
+        printed := print!("hello")
+    "#;
+    TestPurityCtx::analyze(program);
+}
